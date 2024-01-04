10bbf1d03a13b2219af373e5061e08507521747e
elxp101t3r <eliotx10101@outlook.com>
2024-01-05 00:25:21 +0200

    I got a bug

--- app_gui.py
+++ app_gui.py
@@ -1,4 +1,6 @@
 from turtle import Turtle, Screen
+import pandas as pd
+
 IMAGE = 'europe.gif'
 
 
@@ -16,4 +18,17 @@ class QuestionBox(Turtle):
     def __init__(self):
         super().__init__()
         self.screen = Screen()
-        self.answer = self.screen.textinput(title="Guess the Country", prompt="What's another country name?")
+        self.data = pd.read_csv('eu_countries.csv')
+        
+        self.count_correct = 0
+        self.answer = self.screen.textinput(title=f"{self.count_correct}/36 Correct Countries", prompt="What's another country name?")
+        self.data_l = list(self.data['country'])
+        for self.country in self.data_l:
+            if self.country == self.answer:
+                self.count_correct += 1
+                
+                print(self.count_correct)
+            
+            
+            
+        
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from turtle import Turtle, Screen
 from app_gui import Window_app
-import pandas as pd
 
-w = Window_app()
+
+GAME_ON = True
+while GAME_ON:
+    w = Window_app()
+
