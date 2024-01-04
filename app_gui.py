cbdf48da86a34d40122ccf82d8478ce96134a770
elxp101t3r <eliotx10101@outlook.com>
2024-01-04 20:33:48 +0200

    start with game functionality

--- app_gui.py
+++ app_gui.py
@@ -9,3 +9,11 @@ class Window_app(Turtle):
         self.screen.title('Europe Countries Quiz')
         self.screen.addshape(IMAGE)
         self.shape(IMAGE)
+        self.question_box = QuestionBox()
+        
+
+class QuestionBox(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.screen = Screen()
+        self.answer = self.screen.textinput(title="Guess the Country", prompt="What's another country name?")
--- main.py
+++ main.py
@@ -2,7 +2,3 @@ from app_gui import Window_app
 import pandas as pd
 
 w = Window_app()
-
-df = pd.read_csv('eu_countries.csv')
-print(df)
-df.to_excel('data.xlsx')
