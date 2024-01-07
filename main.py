fb43cf1afbb4720201802df86ab3ec427d8229b9
elxp101t3r <eliotx10101@outlook.com>
2024-01-07 23:08:32 +0200

    wants to write the texts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 IMAGE = 'europe.gif'
 
 screen = turtle.Screen()
@@ -7,5 +8,17 @@ screen.title('Europe Quiz Game')
 screen.addshape(IMAGE)
 turtle.shape(IMAGE)
 
-answer = screen.textinput(title=f'Guess the Europen country', prompt="What's another country name?")
+count_correct = 0
 
+while True:
+    answer = screen.textinput(title=f'{count_correct}/36 Guess the Europen country', prompt="What's another country name?")
+
+    if answer is not None:
+        title_case = answer.title()
+
+        df = pd.read_csv('eu_countries.csv')
+
+        for x in df['country']:
+            if title_case == x:
+                count_correct += 1
+                
