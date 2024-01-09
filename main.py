566c578631fdf3c9a0f1d5e280f61706df22bf1b
elxp101t3r <eliotx10101@outlook.com>
2024-01-09 22:47:10 +0200

    Completed!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 import pandas as pd
 
 IMAGE = 'europe.gif'
@@ -17,6 +18,14 @@ guessed_countries = []
 while len(guessed_countries) < 36:
     answer = screen.textinput(title=f'{len(guessed_countries)}/36 Countries | Guess the country', prompt='Whats the next country?').title()
 
+    if answer == 'Exit':
+        a_c = set(all_countries)
+        g_c = set(guessed_countries)
+        countries_to_learn = a_c.difference(g_c)
+        df = pd.DataFrame(countries_to_learn, columns=['Missing Countries to Learn'])
+        df.to_csv('countries_to_learn.csv')
+        check_point = True
+        break
     if answer in all_countries:
         guessed_countries.append(answer)
         t = turtle.Turtle()
@@ -25,5 +34,17 @@ while len(guessed_countries) < 36:
         country_data = data[data.country == answer]
         t.goto(int(country_data.cordinate_x), int(country_data.cordinate_y))
         t.write(country_data.country.item(), align='center')
-
-screen.exitonclick()
+if len(guessed_countries) == 36 or check_point == True:
+    screen.clear()
+    screen.setup(width=600, height=550)
+    message = "Congratulations! You have completed the quiz."
+    turtle.hideturtle()
+    turtle.penup()
+    turtle.write(message, move=False, align="center", font=('Comic', 15, 'normal'))
+    time.sleep(1.5)
+    screen.clear()
+    turtle.hideturtle()
+    turtle.penup()
+    turtle.goto(-60,-250)
+    turtle.write(df)
+    screen.exitonclick()
